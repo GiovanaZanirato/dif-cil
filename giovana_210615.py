80a71a7a803c028b565a9b2c988a715a5a222d8c
GiovanaZanirato <84473403+GiovanaZanirato@users.noreply.github.com>
2021-06-16 17:09:57 -0300

    Add files via upload

---
+++ giovana_210615.py
@@ -0,0 +1,39 @@
+#questions 2 and 3
+blue=[]
+for value in range(4,8):
+    blue.append(value)
+print(blue)
+#questions 4 and 5
+cook=list(range(1,11))
+print(cook)
+#question 6
+for value in range(10):
+    print("Giovana")
+#questions 7 and 8
+square=[]
+for value in range(1,11):
+    square.append("Giovana")
+print(square)
+#questions 9 and 10
+bread=[]
+for value in range(5):
+    bread.append("giovana")
+for value in range(5):
+    bread.append("GIOVANA")
+print(bread)
+#questions 11,12 and 13
+print("************************************************")
+baboons=[]
+for value in range(10):
+    baboons.append("giovana")
+print(baboons)
+for value in range(5,10):
+    baboons[value]="GIOVANA"
+print(baboons)
+#questions 14 and 15
+red=[]
+black=1
+for value in range(7):
+    black=black+value
+    red.append(black)
+print(red)
